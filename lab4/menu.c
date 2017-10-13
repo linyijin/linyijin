fe4800cb7dedb39166ce08ee1c53d697ec65df39
951909997 <951909997@qq.com>
2017-10-14 00:08:42 +0800

    14

--- lab4/menu.c
+++ lab4/menu.c
@@ -5,11 +5,45 @@ int Help();
 #define CMD_MAX_LEN 128
 #define DESC_LEN    1024
 #define CMDNUM      10
-static tDataNode head[]=
+static tDataNode
+{ 
+    tLinkTableNode *pNext;
+    char *cmd;
+    char *desc;
+    int (*handler)();
+   // {"help","this is help cmd!",Help,&head[1]},
+    //{"version","menu program v1.0",NULL,NULL}
+}tDataNode;
+tData *FindCmd(tLinkTable head,char cmd)
 {
-    {"help","this is help cmd!",Help,&head[1]},
-    {"version","menu program v1.0",NULL,NULL}
-};
+    tDataNode *pNode=(tDataNode*)GetLinkTableHead(head);
+    while(pNode!=NULL)
+    {
+        if(strcmp(pNode->cmd,cmd)==0)
+            return pNode;
+        pNode=(tDataNode*)GetNextLinkTableNode(head,(tLinkTableNode*)pNode);
+    }
+}
+int ShowAllCmd(tLinTable*head)
+{
+    tDataNode *pNode=(tDataNode*)GetLinkTableHead(head);
+    while(pNode!=NULL)
+    {
+        printf("%s - %s\n",tDataNode->cmd,tDataNode->desc);
+        pNode=(tDataNode*)GetNextLinkTableNode(head,(tLinkNode*)pNode);
+    }
+    return 0;
+}
+int InitMenuData(tLinkTable **ppLinkTable)
+{
+    *ppLinkTable=CreateLinkTable();
+    tDataNode * pNode=(tDataNode*)malloc(sizeof(tDataNode));
+    pNode->data="help";
+    pNode->desc="Menu List";
+    pNode->handler=Help;
+    addLinkTableNode(*ppLinkTable,(*tLinketable *)pNode);
+    
+}
 main()
 {
     while(1)
