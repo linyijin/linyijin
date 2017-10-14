da7383c43957bd5ab5d3ecbc3fea5e15c621333f
951909997 <951909997@qq.com>
2017-10-14 21:32:59 +0800

    15

--- lab4/linktable.c
+++ lab4/linktable.c
@@ -9,3 +9,24 @@ tLinkTableNode * GetNextLinkTableNode(tLinkTable *t,tLinkTableNode *pNode)
 {
     return pNode->pnext;
 }
+tLinkTable * CreateLinkTable()
+{
+    tLinkTable *t;
+    t->pHead->pnext=t->pTail;
+    t->pTail->pnext=NULL;
+    t->SumOfNode=0;
+    return t;
+}
+int addLinkTableNode(tLinkTable *t,tLinkTableNode *pNode)
+{
+    tLinkTableNode *i,*j;
+    pNode->pnext=t->pTail;
+    i=t->pHead;
+    while(i!=t->pTail)
+    {
+        j=i;
+        i=i->pnext;
+    }
+    j->pnext=pNode;
+    return 0;
+}
